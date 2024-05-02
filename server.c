1fca109056d73a4cf7a91ad4748bfd2b57347284
NicolasLONGHI <nico784474@gmail.com>
2024-05-02 14:50:21 +0200

    Ajout modification port + readme

--- server.c
+++ server.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <pthread.h>
+#include <signal.h>
 #include <string.h> // Pour strerror
 #include <errno.h>  // Pour errno
 
 #define NB_MAX_CLIENTS 10
 #define TAILLE_BUFFER 1024
-#define PORT 8888
+#define PORT_PAR_DEFAUT 8888
 
 typedef struct {
     int id;
@@ -23,6 +25,9 @@ client_info clients[NB_MAX_CLIENTS];
 pthread_t threads[NB_MAX_CLIENTS];
 int num_clients = 0;
 int server_socket;
+int afficherMessage = 1;
+const char *nomFichierConfig = "port.txt";
+int port = 8888;
 
 void *nouvelleConnexion(void *arg) {
     int client_id = *((int *)arg);
@@ -43,8 +48,10 @@ void *nouvelleConnexion(void *arg) {
             char messageBufferModifie[TAILLE_BUFFER + 27];
             memset(messageBufferModifie, 0, TAILLE_BUFFER + 27);
             sprintf(messageBufferModifie, "[%s] : %s", clients[client_id].nomUtilisateur, messageBuffer);
-        
-            printf("%s", messageBufferModifie);
+
+            if (afficherMessage == 1) {
+                printf("%s", messageBufferModifie);
+            }
             //Envoi du message à tous les clients
             for (int i = 0; i < num_clients; i++) {
                 if (i != client_id) {
@@ -71,17 +78,66 @@ void entrerCommande(int signum) {
             write(clients[i].socket, messageBufferModifie, strlen(messageBufferModifie));
         }
     }
-    else if (strcmp(commande, "port\n\0") == 0) {
-        //changement de port dans le fichier de configuration
+    else if (strcmp(commande, "show\n\0") == 0) {
+        if (afficherMessage == 0) {
+            afficherMessage = 1;
+            printf("Les messages des clients s'afficheront\n");
+        }
+        else {
+            afficherMessage = 0;
+            printf("Les messages des clients ne s'afficheront plus\n");
+        }
     }
     else if (strcmp(commande, "help\n\0") == 0) {
-        printf("Les commandes disponibles :\n\tmessage : Envoyer un message à tous les clients connectés\n\tport : Modifier le port du serveur\n");
-        printf("\thelp : Connaître toutes les commandes\n\texit : Fermer le serveur\n");
+        printf("Les commandes disponibles :\n\tmessage : Envoyer un message à tous les clients connectés\n");
+        printf("\tshow : Affiche ou n'affiche plus les messages des clients\n\thelp : Connaître toutes les commandes\n\texit : Fermer le serveur\n");
     }
     else if (strcmp(commande, "exit\n\0") == 0) {
+        printf("Fermeture du serveur\n");
         close(server_socket);
         exit(EXIT_SUCCESS);
     }
+}
+
+void lirePort() {
+    int fichier = open(nomFichierConfig, O_RDWR | O_CREAT, 0666); //ouvrir
+    if (fichier == -1) {
+        fprintf(stderr, "Erreur lors de l'ouverture du fichier de configuration\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Lecture du numéro de port
+    char buffer[7];
+    ssize_t checkRead = read(fichier, buffer, 6); //lecture
+    if (checkRead == -1) {
+        fprintf(stderr, "Erreur lors de la lecture du fichier de configuration\n");
+        close(fichier);
+        exit(EXIT_FAILURE);
+    }
+    buffer[checkRead] = '\0';
+
+
+    port = atoi(buffer);
+    if (port < 1 || port > 65535) {
+        printf("Le numéro de port lu n'est pas valide. Utilisation du port par défaut 8888.\n");
+        if (ftruncate(fichier, 0) == -1) { //supprimer le contenu du fichier
+            perror("Erreur lors de la troncature du fichier de configuration");
+            close(fichier);
+            exit(EXIT_FAILURE);
+        }
+        lseek(fichier, 0, SEEK_SET);
+        char portParDefaut[6];
+        snprintf(portParDefaut, sizeof(portParDefaut), "%d", PORT_PAR_DEFAUT);
+        int checkWrite = write(fichier, portParDefaut, strlen(portParDefaut)); //ecrire dans le fichier le port par défaut
+        if (checkWrite == -1) {
+            fprintf(stderr, "Erreur lors de l'écriture du fichier de configuration");
+            close(fichier);
+            exit(EXIT_FAILURE);
+        }
+        port = PORT_PAR_DEFAUT;
+    }
+
+    close(fichier);
 }
 
 int main() {
@@ -89,9 +145,7 @@ int main() {
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_len = sizeof(client_addr);
 
-
-    // Fichier de configuration
-    
+    lirePort();    
 
     server_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (server_socket == -1) {
@@ -102,7 +156,7 @@ int main() {
     //Configuration de l'adresse du serveur
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
+    server_addr.sin_port = htons(port);
 
     int checkBind = bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr));
     if (checkBind == -1) {
@@ -117,7 +171,7 @@ int main() {
         return 1;
     }
 
-    printf("Attente de connexion sur le port %d ...\n", PORT);
+    printf("Attente de connexion sur le port %d ...\n", port);
 
     signal(SIGINT, entrerCommande);
 
